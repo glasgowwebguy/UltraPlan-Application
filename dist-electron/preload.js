2cd34fb10067e97ea73ac95d1e41d9f548263677
J Weir <glasgowwebguy@gmail.com>
2026-01-01 07:53:53 +0000

    Add files via upload

---
+++ dist-electron/preload.js
@@ -0,0 +1,6 @@
+import { contextBridge } from 'electron';
+// Expose minimal APIs to renderer for Electron-specific functionality
+contextBridge.exposeInMainWorld('electronAPI', {
+    platform: process.platform,
+    isElectron: true,
+});
